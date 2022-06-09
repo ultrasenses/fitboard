542a8e823d7043ac1294942d4b46c83e867cbda8
ilyabolotov <ilya.bolotov@trember.com>
2022-06-09 14:15:29 +0300

    [FITBOARD-1] fix: fix code style

--- lib/screens/workouts/bloc/workouts_bloc.dart
+++ lib/screens/workouts/bloc/workouts_bloc.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ultrasenses_fitboard/data/workout_data.dart';
 import 'package:meta/meta.dart';
 
